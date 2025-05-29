190dc1a342baf6d58b154ae8ce03ffa66a62f8c4
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-05-29 08:00:40 +0300

    add projects to dashboard

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -45,6 +45,7 @@ return {
         keys = {
           { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
           { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+          { icon = " ", key = "p", desc = "Projects", action = ":lua Snacks.picker.projects()" },
           { icon = " ", key = "e", desc = "Explorer", action = ":lua Snacks.explorer()" },
           { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
           { icon = " ", key = "r", desc = "Recent Files", action = ":lua Snacks.dashboard.pick('oldfiles')" },
