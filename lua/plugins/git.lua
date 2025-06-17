701ff4bd5b5e2a74e8a97a3fc2ec983d8ac840dc
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-06-17 08:28:01 +0300

    update git config

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -66,6 +66,20 @@ return {
       end,
     },
   },
+  {
+    "gitsigns.nvim",
+    opts = function()
+      Snacks.toggle({
+        name = "Git Signs",
+        get = function()
+          return require("gitsigns.config").config.signcolumn
+        end,
+        set = function(state)
+          require("gitsigns").toggle_signs(state)
+        end,
+      }):map("<leader>uG")
+    end,
+  },
 
   {
     "NeogitOrg/neogit",
