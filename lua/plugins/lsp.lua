8a072a865eec7f8ff9c9fd74f0219086465a2abd
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-07-17 11:20:48 +0300

    add lsp diagnostic util

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -152,33 +152,7 @@ return {
       })
 
       -- Diagnostics
-      vim.diagnostic.config({
-        severity_sort = true,
-        float = { border = "rounded", source = "if_many" },
-        underline = { severity = vim.diagnostic.severity.ERROR },
-        signs = {
-          text = {
-            [vim.diagnostic.severity.ERROR] = "󰅚 ",
-            [vim.diagnostic.severity.WARN] = "󰀪 ",
-            [vim.diagnostic.severity.INFO] = "󰋽 ",
-            [vim.diagnostic.severity.HINT] = "󰌶 ",
-          },
-        } or {},
-        virtual_text = {
-          source = "if_many",
-          -- prefix = "",
-          spacing = 2,
-          format = function(diagnostic)
-            local diagnostic_message = {
-              [vim.diagnostic.severity.ERROR] = diagnostic.message,
-              [vim.diagnostic.severity.WARN] = diagnostic.message,
-              [vim.diagnostic.severity.INFO] = diagnostic.message,
-              [vim.diagnostic.severity.HINT] = diagnostic.message,
-            }
-            return diagnostic_message[diagnostic.severity]
-          end,
-        },
-      })
+      Util.diagnostic.config()
 
       -- LSP setup
       -- require("mason-lspconfig").setup({
