b4152ee4a6344d38c654858200c9ef0dd7a197a8
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-07-17 13:37:14 +0300

    update lsp config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -91,26 +91,12 @@ return {
           -- map("<leader>ss", "<cmd>FzfLua lsp_document_symbols<cr>", "Symbols")
           -- map("<leader>sS", "<cmd>FzfLua lsp_workspace_symbols<cr>", "Symbols (Workspace)")
 
-          -- This function resolves a difference between neovim nightly (version 0.11) and stable (version 0.10)
-          ---@param client vim.lsp.Client
-          ---@param method vim.lsp.protocol.Method
-          ---@param bufnr? integer some lsp support methods only in specific files
-          ---@return boolean
-          local function client_supports_method(client, method, bufnr)
-            if vim.fn.has("nvim-0.11") == 1 then
-              return client:supports_method(method, bufnr)
-            else
-              ---@diagnostic disable-next-line: param-type-mismatch
-              return client.supports_method(method, { bufnr = bufnr })
-            end
-          end
-
           local client = vim.lsp.get_client_by_id(event.data.client_id)
 
           -- -- The following two autocommands are used to highlight references of the
           -- -- word under your cursor when your cursor rests there for a little while.
           -- -- (using snacks.word instead)
-          -- if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+          -- if client and client:supports_method(client, vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
           --   local highlight_augroup = vim.api.nvim_create_augroup("dotvim_lsp-highlight", { clear = false })
           --   vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
           --     buffer = event.buf,
@@ -143,7 +129,7 @@ return {
           end
 
           -- -- Inlay hints (currently using snacks for this)
-          -- if client and client_supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+          -- if client and client:supports_method(client, vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
           --   map("<leader>th", function()
           --     vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = event.buf }))
           --   end, "Toggle Inlay Hints")
