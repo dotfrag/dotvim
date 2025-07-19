2894d596027fcd948bb38661351e77b32a723dd5
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-07-19 20:38:56 +0300

    update diagnostic config

--- lua/util/diagnostic.lua
+++ lua/util/diagnostic.lua
@@ -1,3 +1,5 @@
+-- https://github.com/nvim-lua/kickstart.nvim/pull/1628
+
 ---@class util.diagnostic
 local M = {}
 
@@ -87,11 +89,60 @@ local function setup_dynamic()
   })
 end
 
+local function setup_hover()
+  vim.diagnostic.config({
+    update_in_insert = true,
+    severity_sort = true,
+    -- underline = true,
+    underline = { severity = vim.diagnostic.severity.ERROR },
+    signs = { text = diagnostic_icons },
+  })
+
+  -- Diagnostic configuration similar to VS Code's Error Lens.
+  -- In insert mode, diagnostics are displayed as inline virtual text.
+  -- In normal mode, diagnostics are shown as virtual lines below the affected lines.
+  ---@param enable boolean
+  local function set_virtual_text(enable)
+    vim.diagnostic.config({
+      virtual_text = enable and {
+        source = "if_many",
+        spacing = 2,
+        format = function(diagnostic)
+          return (diagnostic_icons[diagnostic.severity] or "") .. diagnostic.message
+        end,
+      } or false,
+    })
+  end
+
+  set_virtual_text(false)
+
+  vim.api.nvim_create_autocmd("InsertEnter", {
+    callback = function()
+      set_virtual_text(true)
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("ModeChanged", {
+    pattern = "i:*",
+    callback = function()
+      set_virtual_text(false)
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("CursorHold", {
+    callback = function()
+      vim.diagnostic.open_float(nil, { focusable = false, source = "if_many", border = "rounded" })
+    end,
+  })
+end
+
 function M.config()
   if mode == "static" then
     setup_static()
   elseif mode == "dynamic" then
     setup_dynamic()
+  elseif mode == "hover" then
+    setup_hover()
   end
 end
 
