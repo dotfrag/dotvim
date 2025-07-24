5774db353b0e204e5a3af0d5d7b950fc9323c6a7
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-07-24 15:12:50 +0300

    update snacks config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -103,9 +103,9 @@ return {
     { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
     -- find
     { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
-    { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
-    { "<leader>ff", function() Snacks.picker.files() end, desc = "Find Files" },
-    { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Find Git Files" },
+    { "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Config Files" },
+    { "<leader>ff", function() Snacks.picker.files() end, desc = "Files" },
+    { "<leader>fg", function() Snacks.picker.git_files() end, desc = "Git Files" },
     { "<leader>fp", function() Snacks.picker.projects() end, desc = "Projects" },
     { "<leader>fr", function() Snacks.picker.recent() end, desc = "Recent" },
     -- git
