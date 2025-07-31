7de30a7e93140c04c7d2dbbf10c4b322642de67e
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-07-31 13:40:44 +0300

    wip

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,18 +1,21 @@
 vim.pack.add({
   { src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
   { src = "https://github.com/nvim-treesitter/nvim-treesitter", version = "main" },
-  "https://github.com/NMAC427/guess-indent.nvim",
-  "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim",
   "https://github.com/echasnovski/mini.nvim",
   "https://github.com/folke/lazydev.nvim",
   "https://github.com/folke/tokyonight.nvim",
   "https://github.com/kylechui/nvim-surround",
+  "https://github.com/lewis6991/gitsigns.nvim",
   "https://github.com/mason-org/mason.nvim",
+  "https://github.com/NeogitOrg/neogit",
   "https://github.com/neovim/nvim-lspconfig",
+  "https://github.com/NMAC427/guess-indent.nvim",
+  "https://github.com/nvim-lua/plenary.nvim",
   "https://github.com/nvim-treesitter/nvim-treesitter-context",
   "https://github.com/saghen/blink.cmp",
   "https://github.com/stevearc/conform.nvim",
   "https://github.com/stevearc/oil.nvim",
+  "https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim",
   "https://github.com/windwp/nvim-autopairs",
 })
 
@@ -26,6 +29,7 @@ local plugins = {
   "mini",
   "editor",
   "conform",
+  "git",
 }
 
 for _, plugin in pairs(plugins) do
