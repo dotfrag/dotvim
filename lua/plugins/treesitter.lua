0f1194e7d946a6f3f9ff809fafde8e638d5f05c9
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-07-31 12:30:19 +0300

    wip

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,38 @@ vim.pack.add({ {
   version = "main",
 } })
 
-require("nvim-treesitter.configs").setup({
-  ensure_installed = { "bash", "svelte", "typescript", "javascript" },
-  highlight = { enable = true },
+-- https://github.com/nvim-lua/kickstart.nvim/pull/1657#issuecomment-3119533001
+---@param buf integer
+---@param language string
+---@return boolean
+local function attach(buf, language)
+  -- check if parser exists before starting highlighter
+  if not vim.treesitter.language.add(language) then
+    return false
+  end
+  vim.treesitter.start(buf, language)
+  return true
+end
+
+local exclude_parsers = { "csv", "sql" }
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function(args)
+    local buf, filetype = args.buf, args.match
+    local language = vim.treesitter.language.get_lang(filetype)
+    for _, parser in pairs(exclude_parsers) do
+      if language == parser then
+        return
+      end
+    end
+    if not language then
+      return
+    end
+    if attach(buf, language) then
+      return
+    end
+    -- attempt to start highlighter after installing missing language
+    require("nvim-treesitter").install(language):await(function()
+      attach(buf, language)
+    end)
+  end,
 })
