757ddc8b89aaaa06ea95ff1c275b160223c82922
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-07-31 11:12:25 +0300

    wip

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,2 +1,7 @@
 require("guess-indent").setup({})
 require("nvim-surround").setup()
+
+if vim.g.pairs == "autopairs" then
+  vim.pack.add({ "https://github.com/windwp/nvim-autopairs" })
+  require("nvim-autopairs").setup()
+end
