d38c4346753384ddb368fb7fc19a492b4d0dfa28
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-08-04 14:16:23 +0300

    wip

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,10 +3,14 @@ require("snacks").setup({
   words = { enabled = true },
 })
 
+-- stylua: ignore start
+
+-- Words
+vim.keymap.set({ "n", "t" }, "]]", function() Snacks.words.jump(vim.v.count1) end, { desc = "next reference" })
+vim.keymap.set({ "n", "t" }, "[[", function() Snacks.words.jump(-vim.v.count1) end, { desc = "Prev Reference" })
+vim.keymap.set({ "n", "t" }, "<a-n>", function() Snacks.words.jump(vim.v.count1, true) end, { desc = "Next Reference" })
+vim.keymap.set({ "n", "t" }, "<a-p>", function() Snacks.words.jump(-vim.v.count1, true) end, { desc = "Prev Reference" })
+
 -- Lazygit
-vim.keymap.set("n", "<A-g>", function()
-  Snacks.lazygit({ win = { width = 0, height = 0 } })
-end, { desc = "Lazygit" })
-vim.keymap.set("n", "<leader>gg", function()
-  Snacks.lazygit({ win = { width = 0, height = 0 } })
-end, { desc = "Lazygit" })
+vim.keymap.set("n", "<A-g>", function() Snacks.lazygit({ win = { width = 0, height = 0 } }) end, { desc = "Lazygit" })
+vim.keymap.set("n", "<leader>gg", function() Snacks.lazygit({ win = { width = 0, height = 0 } }) end, { desc = "Lazygit" })
