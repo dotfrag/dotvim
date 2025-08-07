e88ea21af5896e0393f7645d85c5ddd5b44cfc67
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-08-07 11:09:03 +0300

    wip

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -106,6 +106,7 @@ autocmd("BufWritePre", {
 --     local lcount = vim.api.nvim_buf_line_count(buf)
 --     if mark[1] > 0 and mark[1] <= lcount then
 --       pcall(vim.api.nvim_win_set_cursor, 0, mark)
+--       vim.cmd([[normal! g`"zv]])
 --     end
 --   end,
 -- })
