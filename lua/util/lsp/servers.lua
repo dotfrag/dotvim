ef3c8c10c42f888e740275b3492551f71e648e49
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-08-07 14:02:51 +0300

    wip

--- lua/util/lsp/servers.lua
+++ lua/util/lsp/servers.lua
@@ -22,18 +22,15 @@ return {
 
   gopls = {},
 
-  pyright = {},
+  basedpyright = {},
   ruff = {
-    keys = {
-      {
-        "<leader>co",
-        -- function()
-        --   vim.lsp.buf.code_action({ apply = true, context = { only = { "source.organizeImports" }, diagnostics = {} } })
-        -- end,
-        "source.organizeImports",
-        "Organize Imports",
-      },
-    },
+    cmd_env = { RUFF_TRACE = "messages" },
+    init_options = { settings = { logLevel = "error" } },
+    keys = { { "<leader>co", "source.organizeImports", "Organize Imports" } },
+    -- disable hover in favor of pyright
+    -- on_attach = function(client)
+    --   client.server_capabilities.hoverProvider = false
+    -- end,
   },
 
   biome = {
