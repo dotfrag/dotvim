38ae81779876f8823c3a371c56033e89dee6a2dc
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-09-17 14:49:33 +0300

    update flash config

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -17,6 +17,14 @@ vim.keymap.set("o", "r", function() require("flash").remote() end, { desc = "Rem
 vim.keymap.set({ "o", "x" }, "R", function() require("flash").treesitter_search() end, { desc = "Treesitter Search" })
 vim.keymap.set("c", "<c-s>", function() require("flash").toggle() end, { desc = "Toggle Flash Search" })
 -- stylua: ignore end
+vim.keymap.set({ "n", "x", "o" }, "<C-Space>", function()
+  require("flash").treesitter({
+    actions = {
+      ["<C-Space>"] = "next",
+      ["<BS>"] = "prev",
+    },
+  })
+end, { desc = "Treesitter Incremental Selection" })
 
 vim.keymap.set("n", "<F5>", vim.cmd.UndotreeToggle)
 
