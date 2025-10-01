f4016ae8420bf42aedf734a9df2e40c0ddfab2c2
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-10-01 14:54:47 +0300

    update conform keymaps

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -59,6 +59,8 @@ require("conform").setup({
   },
 })
 
+vim.keymap.set("n", "<leader>cF", "<cmd>ConformInfo<cr>")
+
 vim.keymap.set("n", "<leader>cf", function()
   require("conform").format({ async = true, lsp_format = "fallback" })
 end)
