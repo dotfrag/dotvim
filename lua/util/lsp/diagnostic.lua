ed66de63638a2c0f6b891be60add37d0ce05142c
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-10-04 12:02:32 +0300

    update diagnostic keymaps

--- lua/util/lsp/diagnostic.lua
+++ lua/util/lsp/diagnostic.lua
@@ -158,9 +158,12 @@ function M.setup(mode)
   end
 
   local diagnostic_goto = function(next, severity)
-    severity = severity and vim.diagnostic.severity[severity] or nil
     return function()
-      vim.diagnostic.jump({ count = next and 1 or -1, float = true, severity = severity })
+      vim.diagnostic.jump({
+        count = (next and 1 or -1) * vim.v.count1,
+        severity = severity and vim.diagnostic.severity[severity] or nil,
+        float = true,
+      })
     end
   end
   vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Line Diagnostics" })
