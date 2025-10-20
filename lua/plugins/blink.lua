5cf950bff80cf976480ba717fa34c08898a19471
dotfrag <17456867+dotfrag@users.noreply.github.com>
2025-10-20 08:41:28 +0300

    make BlinkBinary async

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -6,20 +6,21 @@ vim.api.nvim_create_user_command("BlinkBinary", function()
   vim.notify("blink.cmp: downloading pre-built binary", vim.log.levels.INFO)
   -- local obj = vim.system({ "cargo", "build", "--release" }, { cwd = plugin_path }):wait()
   vim.fn.mkdir(binary_dir, "p")
-  local obj = vim
-    .system({
-      "wget",
-      "-O",
-      binary_path,
-      "https://github.com/Saghen/blink.cmp/releases/latest/download/x86_64-unknown-linux-gnu.so",
-    })
-    :wait()
-  if obj.code == 0 then
-    vim.notify("blink.cmp: downloading complete", vim.log.levels.INFO)
-  else
-    vim.notify("blink.cmp: downloading failed", vim.log.levels.ERROR)
-    vim.print(obj.stderr)
-  end
+  vim.system({
+    "wget",
+    "-O",
+    binary_path,
+    "https://github.com/Saghen/blink.cmp/releases/latest/download/x86_64-unknown-linux-gnu.so",
+  }, { text = true }, function(obj)
+    vim.schedule(function()
+      if obj.code == 0 then
+        vim.notify("blink.cmp: downloading complete", vim.log.levels.INFO)
+      else
+        vim.notify("blink.cmp: downloading failed", vim.log.levels.ERROR)
+        vim.print(obj.stderr)
+      end
+    end)
+  end)
 end, { desc = "Download latest blink pre-built binary" })
 
 if not vim.uv.fs_stat(binary_path) then
