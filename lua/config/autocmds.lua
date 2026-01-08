a8a8b576bbe9949b132b2218f928f9528967f195
dotfrag <17456867+dotfrag@users.noreply.github.com>
2026-01-08 19:01:41 +0200

    move autosort autocmd to autocmds for consistency

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -90,3 +90,14 @@ autocmd("FileType", {
     vim.opt_local.spell = true
   end,
 })
+
+-- Autosort plugins table
+vim.api.nvim_create_autocmd("BufWritePre", {
+  group = augroup("autosort-plugins"),
+  pattern = "lua/plugins/init.lua",
+  callback = function()
+    local cur = vim.api.nvim_win_get_cursor(0)
+    vim.cmd([[g/AUTOSORT: START/+1,/AUTOSORT: END/-1 sort i]])
+    vim.api.nvim_win_set_cursor(0, cur)
+  end,
+})
