a5c4775eb37a02f3bed88f5a722cf77c46befeac
dotfrag <17456867+dotfrag@users.noreply.github.com>
2026-03-22 13:03:08 +0200

    update lazydev config

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -39,6 +39,8 @@ require("lazydev").setup({
     -- See the configuration section for more details
     -- Load luvit types when the `vim.uv` word is found
     { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+    { path = "nvim-lspconfig", words = { "lspconfig.settings" } },
+    { path = "snacks.nvim", words = { "Snacks" } },
   },
 })
 
